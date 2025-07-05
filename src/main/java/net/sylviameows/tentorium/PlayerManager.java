fc4b3a62ad83cc26a3eaf43df73311d64911da0e
Alex <89748403+Alexxiconify@users.noreply.github.com>
2025-07-04 18:59:40 -0600

    0.0.0.1.4

--- src/main/java/net/sylviameows/tentorium/PlayerManager.java
+++ src/main/java/net/sylviameows/tentorium/PlayerManager.java
@@ -1,6 +1,5 @@
 package net.sylviameows.tentorium;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
